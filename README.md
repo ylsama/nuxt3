eae1f8273ee529fba86710c375e62b8a7d03ecbc
ylsama <ylongkaka3@gmail.com>
2022-05-04 23:22:16 +0700

    Rename Tailwind to Docs, remove default README.md

--- README.md
+++
@@ -1,3 +0,0 @@
-# nuxt-starter-v46qyc
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-v46qyc)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,8 @@ import { defineNuxtConfig } from 'nuxt';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['@/assets/styles/styles.css'],
+  css: ['@/assets/css/main.css'],
+  modules: ['@nuxt/postcss8', '@nuxtjs/tailwindcss'],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js'),
