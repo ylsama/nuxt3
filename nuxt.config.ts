7d8914d8f3a8b9f503428b01f749d81fa964ebc5
ylsama <ylongkaka3@gmail.com>
2022-05-04 23:32:28 +0700

    remove module from nuxt config (bc nuxt3 throw error)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   css: ['@/assets/css/main.css'],
-  modules: ['@nuxt/postcss8', '@nuxtjs/tailwindcss'],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js'),
