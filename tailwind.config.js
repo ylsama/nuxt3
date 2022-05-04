60d3f7817567b9346e89f50b55db4039c690d9d4
ylsama <ylongkaka3@gmail.com>
2022-05-04 23:58:51 +0700

    Remove "nuxt.config" sytle check from tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
-    "./nuxt.config.{js,ts}",
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
   ],
   theme: {
     extend: {},
