a93e9206723a04cb31ce47b67a2ec887dbfa9a86
ylsama <ylongkaka3@gmail.com>
2022-05-28 11:11:36 +0700

    add env, meta config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,24 @@
 import { defineNuxtConfig } from 'nuxt';
-
+console.log('ENV', process.env.NODE_ENV);
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  env: {
+    apiUrl: process.env.API_BASE_URL,
+    baseUrl: process.env.BASE_URL,
+    isDemo: process.env.IS_DEMO,
+    apiKey: process.env.API_KEY,
+  },
   css: ['@/assets/css/main.css'],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js'),
     },
+  },
+  head: {
+    title: 'OiZ',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
   },
 });
